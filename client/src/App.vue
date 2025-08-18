19f463bdcee40c94b5814074819db078fb46f145
gusdev0258 <16326176760@edu.udesc.br>
2025-08-18 20:34:53 -0300

    feat: client ui

---
+++ client/src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="wrapper">
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+ul {
+  display: flex;
+  align-items: center;
+  list-style: none;
+}
+.wrapper {
+  display: flex;
+  flex-direction: column;
+}
+</style>
---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <h1>Bora usar Kong!</h1>
+  </main>
+</template>
+<style scoped>
+h1 {
+  font-size: 12rem;
+}
+</style>
